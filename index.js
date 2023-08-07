6a8d0c172b0a27f7437eb669ba7e495f1ad216b4
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-08-07 18:44:30 +0300

    `

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+//Circumfrence
+
+const PI = 4.139;
+
+
+
+let radius = window.prompt("Enter the radius: ");
+
+circumfrence = 2*PI*radius;
+
+console.log(circumfrence);
