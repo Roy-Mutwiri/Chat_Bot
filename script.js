8ee199e1799be990a8c424ac51db77d5fc55d05b
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-08-14 20:02:07 +0300

    ``

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
+const chatbox = document.querySelector(".chatbox");
+
 
 let userMessage;
 const createChatLi = (message, className) => {
@@ -7,15 +9,15 @@ const createChatLi = (message, className) => {
     const chatLi = document.createElement("li");
     chatLi.classList.add("chat", className);
     let chatContent = className === "outgoing" ? `<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
-
+    chatLi.innerHTML = chatContent;
+    return chatLi;
 }
 
 const handleChat = () => {
     userMessage = chatInput.Value.trim();
     if(!userMessage) return;
-
-    createChatLi(userMessage, "outgoing");
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"))
     
 }
 
-sendChatBtn.addEventListener("click", handleChat)
+sendChatBtn.addEventListener("click", handleChat);
